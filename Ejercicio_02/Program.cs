cf685dcb349996075ff844885de1baaa36f88765
Blan25ga <gabriel.bl@hotmail.com.ar>
2026-03-08 13:24:45 -0300

    Cambios en Ejercicios1

--- Ejercicio_02/Program.cs
+++ Ejercicio_02/Program.cs
@@ -23,7 +23,7 @@ int multiplicación;
 Console.WriteLine("Ingresa un numero:");
 
 num1 = int.Parse(Console.ReadLine());
-multiplicación = num1 * 3;
+multiplicación = num1 * num1 * num1;
 Console.WriteLine("El resultado de la multiplicación es:" + multiplicación);
 
 
