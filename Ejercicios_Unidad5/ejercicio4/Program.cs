90cac58a26c3299d6919e99977b7c0dbf3fd21c4
Blan25ga <gabriel.bl@hotmail.com.ar>
2026-03-30 20:53:36 -0300

    Ejercicio 4 finalizado (Busca de numeros primos)

--- Ejercicios_Unidad5/ejercicio4/Program.cs
+++ Ejercicios_Unidad5/ejercicio4/Program.cs
@@ -4,5 +4,25 @@
     {
         //* 4. Hacer un programa que solicite UN número y luego calcule y emita un cartel aclaratorio si el mismo es primo o no es primo.  
         //* Nota: un número es primo cuando es divisible únicamente por 1 y por sí mismo.
+
+        int n, cont = 0;
+
+        Console.WriteLine("Ingresa un número: ");
+        n = int.Parse(Console.ReadLine());
+
+        for (int x = 1; x <= n; x++)
+        {
+            if (n % x == 0)
+            {
+                cont++;
+            }
+        }
+        if (cont == 2)
+        {
+            Console.WriteLine("El numero es PRIMO");
+        }
+        else
+            Console.WriteLine("El numero ingresado no es PRIMO");
+
     }
 }
