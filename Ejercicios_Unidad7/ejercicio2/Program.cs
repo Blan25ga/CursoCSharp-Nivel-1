a4e6459d27b1ef2fa74273d4fcd120cf5c7368c6
Blan25ga <gabriel.bl@hotmail.com.ar>
2026-04-12 21:00:04 -0300

    Ejercicio 2 Finalizado

--- Ejercicios_Unidad7/ejercicio2/Program.cs
+++ Ejercicios_Unidad7/ejercicio2/Program.cs
@@ -1,6 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //2. Hacer un programa que solicite 10  números enteros y los guarde en un vector. 
+        //Luego recorrer ese vector para calcular el promedio. 
+        //Mostrar por pantalla los valores que son mayores al promedio.
 
-//2. Hacer un programa que solicite 10  números enteros y los guarde en un vector. 
-//Luego recorrer ese vector para calcular el promedio. 
-//Mostrar por pantalla los valores que son mayores al promedio.
+        int n;
+
+        int[] numeros = new int[10]; // declaracion de vector de 10 numeros
+
+        Console.WriteLine("Ingresa 10 numeros: ");//*Se coloca primero para que no se repita con cada ingreso de numero.
+        for (int x = 0; x < 10; x++)// solicitud y carga de los 10 numeros.
+        {
+            n = int.Parse(Console.ReadLine());
+            numeros[x] = n;
+        }
+        // calculo de promedio.
+        int cont = 0;
+        for (int x = 0; x < 10; x++)
+        {
+            cont += numeros[x];
+        }
+        int promedio = cont / 10;
+        Console.WriteLine("El promedio de los numeros es: " + promedio);
+
+
+        // recorrer el vector y mostrar los mayores al promedio
+        Console.WriteLine("Los números mayores al promedio son:");
+        //*Se coloca primero para que no se repita dentro del for y quede como anunciante de los resultados.
+        for (int x = 0; x < 10; x++)
+        {
+            if (numeros[x] > promedio)
+            {
+                Console.WriteLine(numeros[x]);
+            }
+        }
+
+    }
+}
